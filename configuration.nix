8ad23c92ea100a18b8bec6fe7bdcbde0fb0f2d6a
Jacob Moody <moody@posixcafe.org>
2023-07-04 13:36:15 -0500

    steam, proton works nicely

--- configuration.nix
+++ configuration.nix
@@ -284,6 +284,8 @@
     wlr.enable = true;
   };
 
+  programs.steam.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
